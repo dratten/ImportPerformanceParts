c31c1585bf879f414025ff34f9f987ee02eb4ac0
mark-mokaya <mark.mokaya@strathmore.edu>
2020-07-02 00:00:18 +0300

    Migrations Fix #3: Payments Foreign Constraints

---
+++ database/migrations/2020_07_01_204555_set_payments_table_foreign_constraints.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SetPaymentsTableForeignConstraints extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade')->change();
+            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade')->change();
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade')->change();
+            $table->foreign('company_id')->references('company_id')->on('companydetails')->onDelete('cascade')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign(['product_id']);
+        $table->dropForeign(['customer_id']);
+        $table->dropForeign(['category_id']);
+        $table->dropForeign(['company_id']);
+    }
+}
