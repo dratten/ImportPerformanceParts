5e662b27d913ce5958b3381e237035c99c6c0e0a
Dalton Zai <32575030+dratten@users.noreply.github.com>
2020-07-02 20:36:30 +0300

    Revert "Migrations  Fix"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('user_id');
+            $table->string('customer_firstname');
+            $table->string('customer_lastname');
+            $table->string('email_address')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('telephone');
+            $table->string('fax');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/migrations/2020_05_14_184150_create_payments_table.php
+++ database/migrations/2020_05_14_184150_create_payments_table.php
@@ -16,10 +16,14 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->bigIncrements('payment_id');
             $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('product_id')->on('products');
             $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('user_id')->on('users');
             $table->boolean('payment_status');         
             $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('category_id')->on('categories');
             $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('company_id')->on('companydetails');
             $table->timestamps();
         });
     }
