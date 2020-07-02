2f5ab825f99a96e0361085d6025b758da9886456
Dalton Zai <32575030+dratten@users.noreply.github.com>
2020-07-02 21:30:00 +0300

    Update 2020_07_01_204555_set_payments_table_foreign_constraints.php

--- database/migrations/2020_07_01_204555_set_payments_table_foreign_constraints.php
+++ database/migrations/2020_07_01_204555_set_payments_table_foreign_constraints.php
@@ -13,12 +13,12 @@ class SetPaymentsTableForeignConstraints extends Migration
      */
     public function up()
     {
-        // Schema::table('payments', function (Blueprint $table) {
-        //     $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade')->change();
-        //     $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade')->change();
-        //     $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade')->change();
-        //     $table->foreign('company_id')->references('company_id')->on('companydetails')->onDelete('cascade')->change();
-        // });
+         Schema::table('payments', function (Blueprint $table) {
+             $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade')->change();
+             $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade')->change();
+             $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade')->change();
+             $table->foreign('company_id')->references('company_id')->on('companydetails')->onDelete('cascade')->change();
+         });
     }
 
     /**
